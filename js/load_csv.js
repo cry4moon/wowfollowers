0b753d571e3c694c45f56e4f29330ae2d2f13c28
cry4moon <cry4moon@naver.com>
2015-01-12 16:00:20 +0900

    ㅠㅠ

--- js/load_csv.js
+++ js/load_csv.js
@@ -17,18 +17,49 @@ var data = csv.read("class_test.csv", new CSVReadProc() {
 });
 */
 
-// tutorial1-raw.js
-var CommentBox = React.createClass({displayName: 'CommentBox',
+// tutorial2.js
+var CommentBox = React.createClass({
   render: function() {
     return (
-      React.createElement('div', {className: "commentBox"},
-        "Hello, world! I am a CommentBox."
-      )
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
     );
   }
 });
-React.render(
-  React.createElement(CommentBox, null),
-  document.getElementById('content')
-);
 
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
