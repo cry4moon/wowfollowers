7b3f513393e4e54c451c7edd153d2d7f9076c4b3
cry4moon <cry4moon@naver.com>
2015-01-12 15:52:27 +0900

    ㅇㅇ

--- js/load_csv.js
+++ js/load_csv.js
@@ -1,4 +1,5 @@
 
+/**
 var test = read.fromCSV('class_test.csv')
 for (var k,v in test) {
 	//Firebase.put(k,v);
@@ -8,12 +9,13 @@ for (var k,v in test) {
 import au.com.bytecode.opencsv.CSVReadProc;
 import java.util.Arrays;
 
-/** React + ReactFire @jsx React.DOM */
+
 var data = csv.read("class_test.csv", new CSVReadProc() {
 	public void procRow(int rowIndex, String... values) {
 		System.out.println(rowIndex + ": " + Arrays.asList(values));
 	}
 });
+*/
 
 // tutorial1.js
 var CommentBox = React.createClass({
@@ -28,6 +30,6 @@ var CommentBox = React.createClass({
 React.render(
   <CommentBox />,
   document.getElementById('content')
-  
+
 );
 
