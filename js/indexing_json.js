98a288ee33ad731b95a77033a35679d3d37df18c
cry4moon <cry4moon@naver.com>
2015-01-14 11:51:06 +0900

    bb

---
+++ js/indexing_json.js
@@ -0,0 +1,65 @@
+var TransformTable = React.createClass({
+  render: function() {
+    var transform = function(item, index) {
+      return <tr key={ index }><td>{ item.val() }</td><td>{ item.from }</td></tr>;
+      };
+    return <table>{ table_info.map(transform) }</table>;
+    }
+});
+
+var root_info = "https://followers.firebaseio.com/";
+
+var table_info = {
+  "tb_strings": { "from" : "tb_strings_upload/" },
+  "tb_class": { "from" : "tb_class_upload/"}
+};
+
+var Firebase = require("firebase");
+var ClassRef = new Firebase("https://followers.firebaseio.com/Deathknight_Blood");
+ClassRef.on("value", function(ClassSnapshot) {
+  var key = ClassSnapshot.val();  // key === "fred"
+  console.log(key);
+  //key = ClassSnapshot.child("name/last").name();  // key === "last"
+  //test merong
+});
+
+var TransformApp = React.createClass({
+  mixins: [ReactFireMixin],
+
+  getInitialState: function() {
+    return {table_info};
+  },
+
+  componentWillMount: function() {
+    var firebaseRef = new Firebase("https://followers.firebaseio.com/db_test/");
+    this.bindAsArray(firebaseRef.limitToLast(25), "items");
+  },
+
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    if (this.state.text && this.state.text.trim().length !== 0) {
+      this.firebaseRefs["items"].push({
+        text: this.state.text
+      });
+      this.setState({text: ""});
+    }
+  },
+
+  render: function() {
+    return (
+      <div>
+        <TransformTable items={ table_info } />
+        <form onSubmit={ this.handleSubmit }>
+          <input onChange={ this.onChange } value={ this.state.text } />
+          <button>{ "Add #" + (this.state.items.length + 1) }</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+React.render(<TransformApp />, document.getElementById("transform_view"));
---
+++ js/mission.js
@@ -0,0 +1,54 @@
+/** React + ReactFire @jsx React.DOM */
+
+var ClassTable = React.createClass({
+  render: function() {
+    var createItem = function(item, index) {
+      return <tr key={ index }><td>{ item.직업 }</td><td>{ item.전문화 }</td></tr>;
+    };
+    var createHeader = function(item, index) {
+      return <tr key={ index }><td>{ item.직업 }</td><td>{ item.전문화 }</td></tr>;
+    };
+    return <table>{ this.props.items.map(createItem) }</table>;
+  }
+});
+
+var ClassApp = React.createClass({
+  mixins: [ReactFireMixin],
+
+  getInitialState: function() {
+    return {items: [], text: ""};
+  },
+
+  componentWillMount: function() {
+    var firebaseRef = new Firebase("https://followers.firebaseio.com/db_test/");
+    this.bindAsArray(firebaseRef.limitToLast(25), "items");
+  },
+
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    if (this.state.text && this.state.text.trim().length !== 0) {
+      this.firebaseRefs["items"].push({
+        text: this.state.text
+      });
+      this.setState({text: ""});
+    }
+  },
+
+  render: function() {
+    return (
+      <div>
+        <ClassTable items={ this.state.items } />
+        <form onSubmit={ this.handleSubmit }>
+          <input onChange={ this.onChange } value={ this.state.text } />
+          <button>{ "Add #" + (this.state.items.length + 1) }</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+React.render(<ClassApp />, document.getElementById("class_view"));
