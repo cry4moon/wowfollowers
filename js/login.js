66ace17b20fb03c4e6cfffd1f0e0b10af746df8d
cry4moon <cry4moon@naver.com>
2015-01-15 11:22:07 +0900

    bvb

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 var ref = new Firebase("https://followers.firebaseio.com");
-ref.authWithOAuthRedirect("google", function(error, authData) {
+ref.authWithOAuthPopup("google", function(error, authData) {
   if (error) {
     console.log("Login Failed!", error);
   } else {
