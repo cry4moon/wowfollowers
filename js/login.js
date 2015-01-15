837bee02dddaaff90d4788af9538f9556534df75
cry4moon <cry4moon@naver.com>
2015-01-15 16:30:10 +0900

    sss

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ var ref = new Firebase("https://followers.firebaseio.com");
 var authData = ref.getAuth();
 if (authData) {
 	console.log("User " + authData.uid + " is logged in with " + authData.provider);
+	console.log("Authenticated successfully with payload:", authData);
 	} else {
 	ref.authWithOAuthRedirect("google", function(error, authData) {
 	  if (error) {
