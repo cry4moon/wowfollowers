94e4762eae50bea1b811aff440135fe860436be2
cry4moon <cry4moon@naver.com>
2015-01-15 14:18:50 +0900

    bb

--- js/class_list.js
+++ js/class_list.js
@@ -1,101 +1,24 @@
-var root_info = "https://followers.firebaseio.com/";
+/** React + ReactFire @jsx React.DOM */
 
-var table_info = '{"table":[' +
-'{"to":"tb_class/","from":"tb_class_upload/" },' +
-'{"to":"tb_strings/","from":"tb_strings_upload/" }' +
-']}';
-
-obj = JSON.parse(table_info);
-
-function logArrayElements(item, index) {
-  var FromRef = new Firebase(root_info + item.from);
-  FromRef.orderByChild("order").once('value', function(data_list) {
-      data_list.forEach(function(data_row) {
-      console.log(data_row.child("key").val());
-    });
-  });
-}
-
-// Note ellision, there is no member at 2 so it isn't visited
-
-/**
-var TransformTable = React.createClass({
+var ClassTable = React.createClass({
   render: function() {
-    var FromRef = new Firebase(root_info + item.from);
-    var DataSet = FromRef.orderByChild("order").once('value', function(data_list) {
-      data_list.forEach(function(data_row) {
-      console.log(data_row.child("key").val());
-    var transform = function(item, index) {
-      return <tr key={ index }><td>{ item.val() }</td><td>{ item.from }</td></tr>;
-      };
-    return <table>{ table_info.map(transform) }</table>;
-    return <table>{ obj.table.map(logArrayElements) }</table>;
-    }
-    var trans2 = function(item, index) {
-      
-    });
-  });
-}
-*/
-
-function trGen(item, index) {
-  var FromRef = new Firebase(root_info + item.from);
-  FromRef.orderByChild("order").once('value', function(data_list) {
-      data_list.forEach(function(data_row) {
-      console.log(data_row.child("key").val());
-    });
-  });
-}
-
-var TransformTable = React.createClass({
-  render: function() {
-    var FromRef = new Firebase(root_info + "tb_class/");
-    var Transform = function() {
-      FromRef.orderByChild("order").once('value', function(data_list) {
-        var RowProcess = function (item, index) {
-          console.log ( item.key() );
-          return <tr key={ index }><td>{ item.key() }</td></tr>;    
-        };
-      console.log (data_list.map(RowProcess));
-      return data_list.map(RowProcess);
-      });
+    var createItem = function(item, index) {
+      return <tr key={ index }><td>{ item.key() }</td><td>{ item.skill }</td></tr>;
     };
-    return <table> { Transform } </table>;
+    return <table>{ this.props.items.map(createItem) }</table>;
   }
 });
 
-// Note ellision, there is no member at 2 so it isn't visited
-//obj.table.map(logArrayElements);
-
-
-
-
-
-var root_info = "https://followers.firebaseio.com/";
-
-var table_info = {
-  "tb_strings": { "from" : "tb_strings_upload/" },
-  "tb_class": { "from" : "tb_class_upload/"}
-};
-
-var ClassRef = new Firebase("https://followers.firebaseio.com/Deathknight_Blood");
-ClassRef.on("value", function(ClassSnapshot) {
-  var key = ClassSnapshot.val();  // key === "fred"
-  console.log(key);
-  //key = ClassSnapshot.child("name/last").name();  // key === "last"
-  //test merong
-});
-
-var TransformApp = React.createClass({
+var ClassApp = React.createClass({
   mixins: [ReactFireMixin],
 
   getInitialState: function() {
-    return <TransformTable items={ table_info } />;
+    return {items: [], text: ""};
   },
 
   componentWillMount: function() {
-    var firebaseRef = new Firebase("https://followers.firebaseio.com/db_test/");
-    this.bindAsArray(firebaseRef.limitToLast(25), "items");
+    var firebaseRef = new Firebase("https://followers.firebaseio.com/tb_class/");
+    this.bindAsArray(firebaseRef.orderByChilid("order").limitToLast(25), "items");
   },
 
   onChange: function(e) {
@@ -115,14 +38,14 @@ var TransformApp = React.createClass({
   render: function() {
     return (
       <div>
-        <TransformTable items={ table_info } />
+        <ClassTable items={ this.state.items } />
         <form onSubmit={ this.handleSubmit }>
           <input onChange={ this.onChange } value={ this.state.text } />
-          <button>{ "Add #" }</button>
+          <button>{ "Add #" + (this.state.items.length + 1) }</button>
         </form>
       </div>
     );
   }
 });
 
-React.render(<TransformApp />, document.getElementById("transform_view"));
+React.render(<ClassApp />, document.getElementById("class_view"));
