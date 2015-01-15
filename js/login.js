1b056b25ce21f7b9030d4693f719293cbb5ef5ee
cry4moon <cry4moon@naver.com>
2015-01-15 16:54:31 +0900

    ddd

--- js/login.js
+++ js/login.js
@@ -1,11 +1,12 @@
-var root_info = "https://followers.firebaseio.com";
+var root_info = "https://followers.firebaseio.com/";
 var ref = new Firebase(root_info);
 var authData = ref.getAuth();
 
 function getUserInfo () {
 	var user_data = ref.child("db_user/" + authData.uid);
+	console.log (user_data);
 	if ( user_data.key() == null ) {
-		var user_ref = new Firebase(root_info + "/db_user/" + authData.uid);
+		var user_ref = new Firebase(root_info + "db_user/" + authData.uid);
 		user_ref.child("locale").set("kor");
 		user_ref.child("profile").set("profile_text");
 	}
